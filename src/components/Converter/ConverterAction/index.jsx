c3585880aaf081a7ce839b3f0b278ef714dc15d5
ruslangalimov <galimov.ruslan.m@gmail.com>
2022-02-21 16:56:28 +0700

    Updates

--- src/components/Converter/ConverterAction/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './ConverterAction.css'
-
-const ConverterAction = ({tableData, convertForms}) => {
-    return <button className="Button" disabled={!tableData.table1 || !tableData.table2} onClick={() => convertForms()}>Start</button>
-}
-
-export default ConverterAction
--- src/components/Converter/ConverterResult/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './ConverterResult.css'
-
-const ConverterResult = ({result}) => {
-    return <textarea value={result} className="Result" rows={12} readOnly />
-}
-
-export default ConverterResult
